d696d3e139e83d05060de81afe99dbfdd0a75985
Jean Carlos <jeancarlosaps@yahoo.com.br>
2017-02-22 00:20:52 -0300

    API Key configuration.

--- movieZup/movieZup/Constants.swift
+++ movieZup/movieZup/Constants.swift
@@ -5,16 +5,27 @@
 //  Created by Jean Carlos on 2/21/17.
 //  Copyright © 2017 Jean Carlos. All rights reserved.
 //
+// Gerar API Key: http://beforethecode.com/projects/omdb/apikey.aspx?
+/*
+ Your API Key:
+ 
+ affee530
+ Example:
+ 
+ http://img.omdbapi.com/?i=tt2294629&apikey=affee530
+ */
 
 import Foundation
 
+let movieParameter =  "Batman" //Coloquei essa constante aqui, só pra saber que devo montar a URL com esse parâmetro imputado pelo usuário.
+
 struct Constants {
     struct Url {
-        static let GetURL = ""
+        static let GetURL = "http://www.omdbapi.com/?t=\(movieParameter)&y=&plot=short&r=json"
+        static let GetURLPost = "http://img.omdbapi.com/?i=tt2294629&apikey=affee530"
     }
     
     struct Key {
-        static let publicKey = ""
-        static let privateKey = ""
+        static let privateKey = "affee530"
     }
 }
